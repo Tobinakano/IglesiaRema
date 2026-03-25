cbac9a5e47f9cadce9e17bdcf50d0902d758ff77
Tobinakano <nicolashdezrdo@gmail.com>
2026-03-24 23:30:59 -0500

    Inicio proyecto Iglesia Rema

--- remanente/inicio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('familia/', views.familiaRemanente, name='familia_remanente'),
-    path('equipos/', views.equipos, name='equipos'),
-]
--- remanente/registro/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def asistencias(request):
-    return render(request, 'asistencia.html')
