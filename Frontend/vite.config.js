6ecb3da4c5b27d030aa55d0b2f3f35ac43595cdb
Tobinakano <nicolashdezrdo@gmail.com>
2026-06-17 16:15:11 -0500

    Actualizar proxy de Vite para apuntar a Render

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://iglesia-rema-backend.onrender.com',
         changeOrigin: true,
       }
     }
